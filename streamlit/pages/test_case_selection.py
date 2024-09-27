61ba7320c5c52dc90d7db9afa3260335289feebf
Aishwarya Patil <patilaishwarya1610@gmail.com>
2024-09-26 20:19:59 -0400

    Deleted unnecessary files

--- streamlit/pages/test_case_selection.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-# from data.gaia_loader import get_test_cases
-
-def show():
-    st.title("Test Case Selection")
-    # test_cases = get_test_cases()
-    # selected_case = st.selectbox("Choose a test case", test_cases['task_id'])
-    # st.session_state['selected_case'] = selected_case
